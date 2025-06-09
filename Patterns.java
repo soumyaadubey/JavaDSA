b619683db8dbeb0c8dc097bf7e3c69ed9964ba1e
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-06-09 11:57:46 -0400

    more patterns

--- Patterns.java
+++ Patterns.java
@@ -76,13 +76,74 @@ public class Patterns {
             System.out.println();
         }
     }
+    public static void pattern9(int n){
+        pattern7(n);
+        pattern8(n);
+    }
+    public static void pattern10(int n) {
+        for (int i=1; i<=2*n-1; i++){
+            int stars=i;
+            if (i>n){
+                stars=2*n-i;
+            }
+
+            for (int j=0; j<stars; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern11(int n){
+        int start =1;
+        for (int i=0; i<n; i++){
+            if (i%2==0){
+                start=1;
+            } else{
+                start=0;
+            }
+            for (int j=0; j<=i; j++){
+                System.out.print(start +" ");
+                start=1-start;
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern12(int n){
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            for (int k=0;k<2*n-2*i ;k++){
+                System.out.print(" ");
+            }
+            for (int l=i; l>=1 ;l--){
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern13(int n){
+        int counter=0;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<=i; j++){
+                counter++;
+                System.out.print(counter +" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern14(int n){
+        for (int i=0; i<n; i++){
+
+        }
+    }
 
 
            
 
     
     public static void main(String[] args) {
-        pattern1(5);
+        /*pattern1(5);
         pattern2(5);
         pattern3(5);
         pattern4(5);
@@ -90,5 +151,10 @@ public class Patterns {
         pattern6(5);
         pattern7(5);
         pattern8(5);
+        pattern9(5);
+        pattern10(5);
+        pattern11(5);
+        pattern12(4);*/
+        pattern13(5);
     }
 }
