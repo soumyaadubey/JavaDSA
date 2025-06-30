24e9b9ae6a5952d0ea22c9e8d29942bdda364275
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-06-30 17:47:32 -0400

    Added Changes

--- TwoDimensionalArray.java
+++ TwoDimensionalArray.java
@@ -1,6 +1,42 @@
 import java.util.Arrays;
 public class TwoDimensionalArray {
+    static int arr[][]=null;
+
+    public static void createArray(int rows, int cols){
+        arr = new int[rows][cols];
+        for (int i=0; i<arr.length; i++){
+            for (int j=0; j<arr[0].length; j++){
+                arr[i][j]=Integer.MIN_VALUE;
+            }
+        }
+    }
+
+    public static void insertValue(int row, int col, int value){
+        if (arr == null) {
+        System.out.println("Array is not initialized.");
+        return;
+        }
+        try{
+            if (arr[row][col]==Integer.MIN_VALUE){
+                arr[row][col]=value;
+                System.out.println("Value inserted");
+                System.out.println(Arrays.deepToString(arr));
+            } else{
+                System.out.println("Array cell already occupied");
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Index cannot be accessed");
+        }
+    }
+
+
+
+
+
+
+
     public static void main(String[] args) {
+    /* 
     //Declare
     int[][] int2Darr;
     //Instantiate
@@ -19,6 +55,9 @@ public class TwoDimensionalArray {
     //This takes O(1) time complexity
     System.out.println(Arrays.deepToString(s2DArray));
 
+    */
+
+    insertValue(1, 3, 300);
 
     }
     
