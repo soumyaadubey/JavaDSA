8c9fc82311c89f2f058c3fd527cb6a6c916754e4
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-06-30 22:58:12 -0400

    Added Changes

--- TwoDimensionalArray.java
+++ TwoDimensionalArray.java
@@ -29,6 +29,51 @@ public class TwoDimensionalArray {
         }
     }
 
+    public static void accessCell(int row, int col){
+        System.out.println("Accessing row "+ row+" and column "+col);
+        try {
+            System.out.println(arr[row][col]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid index entered");
+        }
+        
+    }
+
+    public static void traverseArray(){
+        try {
+            for (int i=0; i<arr.length; i++){
+                for(int j=0; j<arr[0].length; j++){
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            System.out.println("Array does not exist");
+        }
+    }
+
+    public static void searchArray(int value){
+        for (int i=0; i<arr.length; i++){
+                for(int j=0; j<arr[0].length; j++){
+                    if (arr[i][j]==value){
+                        System.out.println("Value found at index "+i+" "+j );
+                        return;
+                    }
+
+                }
+            }
+        System.out.println("Value not found");
+    }
+
+    public static void deleteIndex(int row, int col){
+        try {
+            arr[row][col]=Integer.MIN_VALUE;
+            System.out.println("Value successfully deleted. Current array is "+Arrays.deepToString(arr));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index does not exist");
+        }
+    }
+
 
 
 
@@ -56,8 +101,19 @@ public class TwoDimensionalArray {
     System.out.println(Arrays.deepToString(s2DArray));
 
     */
+    createArray(2, 2);
+    insertValue(0, 0, 10);
+    insertValue(0, 1, 20);
+    insertValue(1, 0, 100);
+    insertValue(1, 1, 200);
+    //Accessing an element requires O(1) time complexity
+    accessCell(0, 1);
 
-    insertValue(1, 3, 300);
+    traverseArray();
+
+    searchArray(2000);
+
+    deleteIndex(0, 0);
 
     }
     
