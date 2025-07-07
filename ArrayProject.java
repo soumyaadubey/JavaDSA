beb23dc0153421c05bc5c33cf1201b95cfe329f6
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-07-07 13:27:27 -0400

    Added Changes

--- ArrayProject.java
+++ ArrayProject.java
@@ -1,7 +1,30 @@
 import java.util.*;
 public class ArrayProject {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+         System.out.print("How many days' temperature? ");
+         int numDays= scan.nextInt();
+         int[] temps = new int[numDays];
+         int sum=0;
+         for (int i=0; i<numDays; i++){
+            System.out.print("Day "+(i+1)+"'s high temp: ");
+            temps[i] = scan.nextInt();
+            sum+=temps[i];  
+         }
+
+         int above=0;
+
+         double average = sum/numDays;
+         for (int i=0; i<numDays; i++){
+            if (temps[i]>average){
+                above++;
+            }
+         }
+         System.out.println();
+         System.out.println("Average Temperature: "+average);
+         System.out.println("Number of days above average: "+above);
+         
+        scan.close();
     }
     
 }
