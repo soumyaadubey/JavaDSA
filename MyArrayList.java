7830daf111c8264f900e711ae441c580b10e04f3
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-07-11 15:33:41 -0400

    Added Changes

---
+++ MyArrayList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> stringarrlist= new ArrayList<String>(Arrays.asList("adam","eve"));
+        ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(10,30,20,60,70,80));
+        //Adding elements in ArrayList
+        numbers.add(50);
+        numbers.add(40);
+        System.out.println(numbers);
+        stringarrlist.add("lilith");
+        stringarrlist.add(1,"lucifer");
+        System.out.println(stringarrlist );
+        //Accessing elements in arraylist
+        System.out.println(stringarrlist.get(2));
+        //Traversing an arraylist
+        //for loop
+        for (int i=0; i<numbers.size(); i++){
+            System.out.println(numbers.get(i));
+        }
+        //for each loop
+        for (int i: numbers){
+            System.out.println(i);
+        }
+
+
+
+    }
+}
