fdbb65aa1a5f70e8ce700d5e82dadac19c44a39d
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-07-11 15:45:22 -0400

    Added Changes

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 public class MyArrayList {
     public static void main(String[] args) {
         ArrayList<String> stringarrlist= new ArrayList<String>(Arrays.asList("adam","eve"));
         ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(10,30,20,60,70,80));
+
         //Adding elements in ArrayList
         numbers.add(50);
         numbers.add(40);
@@ -11,8 +13,10 @@ public class MyArrayList {
         stringarrlist.add("lilith");
         stringarrlist.add(1,"lucifer");
         System.out.println(stringarrlist );
+
         //Accessing elements in arraylist
         System.out.println(stringarrlist.get(2));
+
         //Traversing an arraylist
         //for loop
         for (int i=0; i<numbers.size(); i++){
@@ -22,6 +26,15 @@ public class MyArrayList {
         for (int i: numbers){
             System.out.println(i);
         }
+        //iterator
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        
+        //Searching an element
+
+
 
 
 
