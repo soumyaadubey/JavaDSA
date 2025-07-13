c26eee5985ac6d540e070b34b8f1dfaeb456b3cd
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-07-12 22:30:01 -0400

    Added Changes

--- Arrays/CodingExercise4.java
+++ Arrays/CodingExercise4.java
@@ -1,3 +1,16 @@
 public class CodingExercise4 {
-    
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,6};
+        System.out.println(findMissingNumberInArray(arr));
+    }
+
+    public static int findMissingNumberInArray(int[] arr) {
+        int n=arr.length+1;
+        int sum=0;
+        for (int i=0; i<arr.length; i++){
+            sum+=arr[i];
+        }
+        int expectedSum = (n)*(n+1)/2;
+        return expectedSum-sum;
+    }
 }
